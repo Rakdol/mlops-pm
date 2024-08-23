ab8fd67d0d7198d33322da26307bd95d5cd0e036
Rakdol <seongmun91@gmail.com>
2024-08-23 19:23:52 +0900

    add: evaluate features

---
+++ machine_detector/src/ml/evaluate/src/configurations.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+import os
+from logging import getLogger
+
+from constants import PLATFORM_ENUM
+
+logger = getLogger(__name__)
+
+
+class PlatformConfigurations:
+    platform = os.getenv("PLATFORM", PLATFORM_ENUM.DOCKER.value)
+    if not PLATFORM_ENUM.has_value(platform):
+        raise ValueError(
+            f"PLATFORM must be one of {[v.value for v in PLATFORM_ENUM.__members__.values()]}"
+        )
+
+
+class DataConfigurations:
+
+    TRAIN_FILE = "train.csv"
+    TEST_FILE = "test.csv"
+    ORIGIN_FILE = "machine failure.csv"
+    LOCAL_FILE_PATH = "/opt/data/raw/total_data.csv"
+
+
+class FeatureConfigurations:
+    TARGET = "machine_failure"
+    NUM_FEATURES = [
+        "air_temperature",
+        "process_temperature",
+        "rotational_speed",
+        "torque",
+        "tool_wear",
+    ]
+
+    BIN_FEATURES = ["twf", "hdf", "pwf", "osf", "rnf"]  # Binary
+    CAT_FEATURES = ["machine_type"]
+    DROP_FEATURES = ["id", "UDI"]
+
+    DOMAIN_FEATURES = [
+        "product_id",
+        "tool_wear",
+        "torque",
+        "rotational_speed",
+        "air_temperature",
+        "process_temperature",
+    ]
+
+    TRANS_FEATURES = [
+        "air_process_diff",
+        "speed_power",
+        "torque_power",
+        "tool_process",
+        "temp_ratio",
+    ]
+
+    FRAME_FEATURES = (
+        CAT_FEATURES + NUM_FEATURES + BIN_FEATURES + TRANS_FEATURES + ["product_id_num"]
+    )
+
+    LABELS = {0: "normal", 1: "failure"}
+
+
+logger.info(f"{PlatformConfigurations.__name__}: {PlatformConfigurations.__dict__}")
+logger.info(f"{DataConfigurations.__name__}: {DataConfigurations.__dict__}")
+logger.info(f"{FeatureConfigurations.__name__}: {FeatureConfigurations.__dict__}")
