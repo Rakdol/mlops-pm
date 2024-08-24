2acbd7a4bdbb4034b55317e2be12ae9a6ab6d987
Rakdol <seongmun91@gmail.com>
2024-08-25 01:53:55 +0900

    modfied: feature confiure

--- machine_detector/src/configurations.py
+++ machine_detector/src/configurations.py
@@ -16,3 +16,52 @@ class DBConfigurations:
         "POSTGRES_SERVER", "172.17.0.1"
     )  # default docker bridge
     sql_alchemy_database_url = f"postgresql://{postgres_username}:{postgres_password}@{postgres_server}:{postgres_port}/{postgres_db}"
+
+
+class FeatureConfigurations:
+    TARGET = "machine_failure"
+    NUM_FEATURES = [
+        "air_temperature",
+        "process_temperature",
+        "rotational_speed",
+        "torque",
+        "tool_wear",
+    ]
+
+    BIN_FEATURES = ["twf", "hdf", "pwf", "osf", "rnf"]  # Binary
+    CAT_FEATURES = ["machine_type"]
+    DROP_FEATURES = ["id", "UDI"]
+
+    DOMAIN_FEATURES = [
+        "product_id",
+        "tool_wear",
+        "torque",
+        "rotational_speed",
+        "air_temperature",
+        "process_temperature",
+    ]
+
+    TRANS_FEATURES = [
+        "air_process_diff",
+        "speed_power",
+        "torque_power",
+        "tool_process",
+        "temp_ratio",
+    ]
+
+    FRAME_FEATURES = (
+        CAT_FEATURES + NUM_FEATURES + BIN_FEATURES + TRANS_FEATURES + ["product_id_num"]
+    )
+
+    LABELS = {0: "normal", 1: "failure"}
+
+
+class ModelConfigurations:
+    ONNX_FILE_NAME = "machine_rf_0.onnx"
+
+
+class ObjectStoreConfigurations:
+    mlflow_s3_endpoint = os.getenv("MLFLOW_S3_ENDPOINT_URL", "http://localhost:9000")
+    mlflow_tracking_uri = os.getenv("MLFLOW_TRACKING_URI", "http://localhost:5001")
+    aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID", "minio")
+    aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY", "miniostorage")
