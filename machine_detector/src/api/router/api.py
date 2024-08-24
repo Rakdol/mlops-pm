f6867d81e1d9c32b165e92d597fcc006e711967f
Rakdol <seongmun91@gmail.com>
2024-08-25 02:13:29 +0900

    Update api.py

--- machine_detector/src/api/router/api.py
+++ machine_detector/src/api/router/api.py
@@ -1,34 +1,65 @@
-import onnxruntime as ort
-import numpy as np
+import uuid
+from logging import getLogger
+from typing import Any, Dict, List
 
-from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
+from fastapi import APIRouter
+from src.infer.predict import Data, classifier
 
-from src.db import cruds, schemas
-from src.db.database import get_db
-
-
+logger = getLogger(__name__)
 router = APIRouter()
 
 
-# ONNX 모델 로드
-ort_session = ort.InferenceSession("model.onnx")
+@router.get("/health")
+def health() -> Dict[str, str]:
+    return {"health": "ok"}
 
 
-@router.get("/projects/all")
-async def predict(input_data: dict):
-    input_name = ort_session.get_inputs()[0].name
-    output_name = ort_session.get_outputs()[0].name
-    data = np.array(input_data["input_data"]).astype(np.float32)
-    data = data.reshape((data.shape[0], 28, 28))
+@router.get("/metadata")
+def metadata() -> Dict[str, Any]:
+    return {
+        "data_type": "float32",
+        "data_structure": "(1,4)",
+        "data_sample": Data().data,
+        "prediction_type": "float32",
+        "prediction_structure": "(1,3)",
+        "prediction_sample": [0.97093159, 0.01558308, 0.01348537],
+    }
 
-    # 추론 실행
-    outputs = ort_session.run(None, {input_name: data})
 
-    return {"predictions": outputs[0].tolist()}
+@router.get("/label")
+def label() -> Dict[int, str]:
+    return classifier.label
 
 
-if __name__ == "__main__":
-    import uvicorn
+@router.get("/predict/test")
+def predict_test() -> Dict[str, List[float]]:
+    job_id = str(uuid.uuid4())
+    prediction = classifier.predict(data=Data().data)
+    prediction_list = list(prediction)
+    logger.info(f"test {job_id}: {prediction_list}")
+    return {"prediction": prediction_list}
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+@router.get("/predict/test/label")
+def predict_test_label() -> Dict[str, str]:
+    job_id = str(uuid.uuid4())
+    prediction = classifier.predict_label(data=Data().data)
+    logger.info(f"test {job_id}: {prediction}")
+    return {"prediction": prediction}
+
+
+@router.post("/predict")
+def predict(data: Data) -> Dict[str, List[float]]:
+    job_id = str(uuid.uuid4())
+    prediction = classifier.predict(data.data)
+    prediction_list = list(prediction)
+    logger.info(f"{job_id}: {prediction_list}")
+    return {"prediction": prediction_list}
+
+
+@router.post("/predict/label")
+def predict_label(data: Data) -> Dict[str, str]:
+    job_id = str(uuid.uuid4())
+    prediction = classifier.predict_label(data.data)
+    logger.info(f"test {job_id}: {prediction}")
+    return {"prediction": prediction}
