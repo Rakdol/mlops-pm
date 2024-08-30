63f23c8c8b2809fe1c1623f9a3977023a8458952
Rakdol <seongmun91@gmail.com>
2024-08-30 18:25:29 +0900

    modify: data and classifier

--- machine_detector/src/infer/predict.py
+++ machine_detector/src/infer/predict.py
@@ -1,87 +1,48 @@
 import os
-import json
-from typing import List, Optional
+import sys
+from typing import List, Dict, Union, Optional
 
-import mlflow
 import joblib
 import numpy as np
 import pandas as pd
 import onnxruntime as rt
+from pydantic import BaseModel
 
-from src.transformers import get_input_pipeline
-from machine_detector.src.configurations import (
+from pathlib import Path
+
+
+from src.configurations import (
     FeatureConfigurations,
     ModelConfigurations,
 )
+from utils.logger import logging
 
-from typing import Dict, List, Sequence
 
+pipe_path = str(Path(__file__).resolve().parents[0])
+sys.path.append(pipe_path)
 
-from pydantic import BaseModel
+from src.transformers import get_input_pipeline
 
 
 class Data(BaseModel):
-    data: List[List[float]] = [[5.1, 3.5, 1.4, 0.2]]
+    data: List[List[Union[str, int, float]]] = [
+        ["L49624", "L ", 299.2, 308.6, 1267, 40.4, 76.0, 0, 0, 0, 0, 0]
+    ]
 
 
 class Classifier(object):
     def __init__(
         self,
-        model_filepath: str,
-        label_filepath: str,
-    ):
-        self.model_filepath: str = model_filepath
-        self.label_filepath: str = label_filepath
-        self.classifier = None
-        self.label: Dict[str, str] = {}
-        self.input_name: str = ""
-        self.output_name: str = ""
-
-        self.load_model()
-        self.load_label()
-
-    def load_model(self):
-        logging.info(f"load model in {self.model_filepath}")
-        self.classifier = rt.InferenceSession(self.model_filepath)
-        self.input_name = self.classifier.get_inputs()[0].name
-        self.output_name = self.classifier.get_outputs()[0].name
-        logging.info(f"initialized model")
-
-    def load_label(self):
-        logging.info(f"load label in {self.label_filepath}")
-        with open(self.label_filepath, "r") as f:
-            self.label = json.load(f)
-        logging.info(f"label: {self.label}")
-
-    def predict(self, data: List[List[int]]) -> np.ndarray:
-        np_data = np.array(data).astype(np.float32)
-        prediction = self.classifier.run(None, {self.input_name: np_data})
-        output = np.array(list(prediction[1][0].values()))
-        logging.info(f"predict proba {output}")
-        return output
-
-    def predict_label(self, data: List[List[int]]) -> str:
-        prediction = self.predict(data=data)
-        argmax = int(np.argmax(np.array(prediction)))
-        return self.label[str(argmax)]
-
-
-classifier = Classifier(
-    model_filepath=ModelConfigurations().model_filepath,
-    label_filepath=ModelConfigurations().label_filepath,
-)
-
-
-class RFClassifier(object):
-    def __init__(
-        self,
         pipeline_directory: str,
+        pipe_file_name: str,
         model_directory: str,
         onnx_file_name: Optional[str],
         providers: List[str] = ["CPUExecutionProvider"],
     ):
-        self.preprocess_pipeline = self.get_input_pipe(pipeline_directory)
-        self.onnx_session = self.get_onnx_model(
+        self.preprocess_pipeline = self.load_pipeline(
+            pipeline_directory, pipe_file_name
+        )
+        self.onnx_session = self.load_model(
             model_directory=model_directory,
             onnx_file_name=onnx_file_name,
             providers=providers,
@@ -89,41 +50,60 @@ class RFClassifier(object):
         self.onnx_input_name = self.onnx_session.get_inputs()[0].name
         self.onnx_label_name = self.onnx_session.get_outputs()[0].name
         self.labels = FeatureConfigurations.LABELS
+        self.columns = FeatureConfigurations.RAW_FEATURES
 
-    def get_input_pipe(self, pipeline_directory: str):
-        logging.info(f"loading pipeline in {pipeline_directory}")
-        pipe_path = mlflow.artifacts.download_artifacts(
-            os.path.join("s3://mlflow", pipeline_directory)
-        )
+    def load_pipeline(self, pipeline_directory: str, pipe_file_name: str):
+        pipe_path = os.path.join(pipeline_directory, pipe_file_name)
+
+        # pipe_path = mlflow.artifacts.download_artifacts(
+        #     os.path.join("s3://mlflow", pipeline_directory)
+        # )
+        logging.info("... pipeline loading ...")
         pipe = joblib.load(pipe_path)
-        logging.info(f"loaded pipeline")
+        logging.info("... pipeline loaded ...")
+
         return pipe
 
-    def get_onnx_model(
+    def load_model(
         self, model_directory: str, onnx_file_name: str, providers: List[str]
     ):
         onnx_directory = os.path.join(model_directory, onnx_file_name)
-        onnx = mlflow.artifacts.download_artifacts(
-            os.path.join("s3://mlflow", onnx_directory)
-        )
+        logging.info("... onnx model loading ...")
+        sess = rt.InferenceSession(onnx_directory, providers=providers)
+        logging.info("... onnx model loaded ...")
 
-        sess = rt.InferenceSession(onnx, providers=providers)
-        logging.info(f"initialize onnx model")
         return sess
 
-    def transform(self, x: pd.DataFrame):
+    def transform(self, x: List[List[Union[str, int, float]]]):
+
+        logging.info("... input data is trasnforming ...")
+        x_frame = pd.DataFrame(x, columns=self.columns)
         x_transformed = (
-            self.preprocess_pipeline.transform(x).to_numpy().astype(np.float32)
+            self.preprocess_pipeline.transform(x_frame).to_numpy().astype(np.float32)
         )
+        logging.info("... input data has been trasnformed ...")
         return x_transformed
 
     def predict(self, x: pd.DataFrame):
 
+        logging.info("... model is predicting ...")
         pred = self.onnx_session.run([self.onnx_label_name], {self.onnx_input_name: x})
+        logging.info("... model has been predicted ...")
 
         return [p for p in pred[0].tolist()]
 
     def predict_label(self, x: pd.DataFrame):
-
+        logging.info("... model is predicting for label ...")
         pred = self.onnx_session.run([self.onnx_label_name], {self.onnx_input_name: x})
+        logging.info("... model has been predicted for label ...")
         return [str(self.labels[p]) for p in pred[0].tolist()]
+
+
+print("current PATH ::-----", os.getcwd())
+classifier = Classifier(
+    pipeline_directory=ModelConfigurations().PIPELINE_DIRECTORY,
+    pipe_file_name=ModelConfigurations().PIPE_FILE_NAME,
+    model_directory=ModelConfigurations().MODEL_DIRECTORY,
+    onnx_file_name=ModelConfigurations().ONNX_FILE_NAME,
+)
+logging.info("Classifier sucessfully loaded")
