3e1ed4aaf26692ed49d9e122933db740007df4d5
Rakdol <seongmun91@gmail.com>
2024-08-31 03:02:37 +0900

    remove comment

--- machine_detector/load_test/locustfile.py
+++ machine_detector/load_test/locustfile.py
@@ -13,18 +13,5 @@ class OnnxUser(HttpUser):
         }
         self.client.post("/v0.1/api/predict/", json=input_data)
 
-
-# class NonOnnxUser(HttpUser):
-#     wait_time = between(1, 5)
-
-#     @task
-#     def predict_non_onnx(self):
-#         input_data = {"input_data": [[0.5] * 784]}
-#         self.client.post("/predict/", json=input_data)
-
-
 # # ONNX 서버에 대한 Locust 실행
 # locust -f locustfile.py OnnxUser --host=http://localhost:8000
-
-# # 비ONNX 서버에 대한 Locust 실행
-# locust -f locustfile.py NonOnnxUser --host=http://localhost:8001
